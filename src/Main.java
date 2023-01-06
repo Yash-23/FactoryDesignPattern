7758bfeb7b1b9011a0602d0070f02ceb6d4efd89
Yash Sakle <yash.sakle23@gmail.com>
2023-01-06 11:08:25 +0530

    Feat: Created object of concrete class by passing information.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AccountFactory bankAcc = new AccountFactory();
+
+        BankAccount acc1 = AccountFactory.createAccount("Personal");
+        acc1.create();
+
+        BankAccount acc2 = AccountFactory.createAccount("Cooperate");
+        acc2.create();
+
+        BankAccount acc3 = AccountFactory.createAccount("Business");
+        acc3.create();
     }
 }
